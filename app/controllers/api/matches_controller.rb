dae5b58399e0371bbc7df11deba083c057778760
winstonbarbe <winston.barbe@gmail.com>
2020-11-13 10:03:04 -0500

    adds sorting of matches

--- app/controllers/api/matches_controller.rb
+++ app/controllers/api/matches_controller.rb
@@ -3,8 +3,8 @@ class Api::MatchesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @received_matches = current_user.received_matches
-    @mutual_matches = current_user.mutual_matches
+    @received_matches = current_user.received_matches.sort_by { |match| match.created_at }.reverse
+    @mutual_matches = current_user.mutual_matches.sort_by { |match| match.created_at }.reverse
     # @mutual_matches = current_user.mutual_matches
 
     render "index.json.jb"
--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,7 @@ class Api::SessionsController < ApplicationController
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id, user_age: user.birth_date }, status: :created
     else
       render json: {}, status: :unauthorized
     end
