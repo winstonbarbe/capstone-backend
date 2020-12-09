1282812d6562cfef8a6607011a8e74dfee3e1f7c
winstonbarbe <winston.barbe@gmail.com>
2020-12-09 10:23:51 -0500

    pushes to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
 
     post "/sessions" => "sessions#create"
     
+    get "/*path" => proc { [200, {}, [ActionView::Base.new.render(file: 'public/index.html')]] } 
   end
 end
